22828dfef62142c3d2d1f593da8b26c183183304
luizrcs <luizrodrigo40@gmail.com>
2021-12-07 18:51:24 -0300

    Day 4

--- bin/main/Utils.kt
+++
@@ -1,5 +0,0 @@
-import kotlin.io.path.*
-
-fun readInputLines(name: String) = Path("input/$name.txt").readLines()
-
-fun checkEquals(value: Any, expected: Any) = check(value == expected) { "Expected $expected, got $value" }
